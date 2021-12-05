20973a9ee2a606e0caf739c3280e946d56926fa2
Azii <violetlovato05@gmail.com>
2021-12-05 23:27:18 +0800

    Done, but I can't win

--- lottery.py
+++ lottery.py
@@ -1,17 +1,20 @@
 import random
 
-guess = int(input("Give a number(0-9): "))
-system = random.randint(0,9)
-game = 'yes'
+name = input("Player, enter your name: ")
+game = 'y'
 while game[0] == 'y':
-    print(f"You guessed,{guess}, and the system generated, {system}")
+    guess1,guess2,guess3 = input("Give 3 numbers(0-9): ").split(',') #Put a comma each number fls.
+    guess = int(guess1),int(guess2),int(guess3)
+    system = random.randint(0,9),random.randint(0,9),random.randint(0,9)
+    print(f"You guessed {guess}, and the system generated {system}")
     if guess == system:
         print("Winner!")
+        game = input("Another game?(type y or n):")
     elif guess != system:
         print("You lose!")
-        print("Try again")
-        game = input("Another game?(type y or n)")
+        print(f"Try again, {name}")
+        game = input("Another game?(type y or n):")
     else:
+        game = input("Another game?(type y or n):")
         game == 'n'
-    
-
+        break  
