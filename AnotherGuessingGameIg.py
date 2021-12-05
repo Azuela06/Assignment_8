4cf4943839f9765bc63858f6cb44d540a7da6f21
Azii <violetlovato05@gmail.com>
2021-12-06 00:21:39 +0800

    Done with some revisions

--- AnotherGuessingGameIg.py
+++ AnotherGuessingGameIg.py
@@ -1,15 +1,17 @@
 import random
 
+user = input("What is your name user number 1?: ")
+print(f"Hello, welcome to another guessing game {user}")
+
 computer = random.randint(0,100)
 Start = True
 while Start:
     Guess = int(input("Guess a number from 0-100: "))
-    print(f"{Guess}, {computer}")
     if Guess == computer:
         print ("Congratulations! You have guessed it correctly")
         break
     elif Guess > computer:
-        print ("The number generated is lower than that")
+        print ("The number generated is lesser than that")
     else:
         Guess < computer
         print ("The number generated is greater than that")
